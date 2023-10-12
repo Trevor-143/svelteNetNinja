60c9c2106c26f33def9d6e5d0ce04a9df5027152
Trevor_143 <trevorarapu2@gmail.com>
2023-10-12 10:37:48 +0300

    lesson 2 complete

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div>
+	<h1>Welcome to your library svelte project</h1>
+	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+        flex-direction: column;
+	}
+</style>
